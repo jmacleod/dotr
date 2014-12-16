2caa61b10f0f8a63604d48fe0564ed1ed498c957
Jared MacLeod <jmacleod@spotify.com>
2014-12-15 21:37:31 -0500

    Initial check-in of first attempt at this

---
+++ dotr.py
@@ -0,0 +1,337 @@
+import sys
+import collections
+
+colors = ['red','green','blue','black','purple']
+
+war_status = ['early','mid','mid','late']
+
+
+class DarknessSpreadsCard:
+	def __init__(self , actions):
+		self.actions = actions
+
+class General:
+	def __init__(self, location, name, hits):
+		self.location = location
+		self.name = name
+		self.total_hits = hits
+		self.remaining_hits = hits
+		location.general.add(self)
+
+class Crystal:
+	def __init__(self,location):
+		self.location = location
+		location.add(self)
+
+class Minion:
+	def __init__(self, color,location):
+		if color in colors:
+			self.color = color
+			self.location = location
+			location.add(self)
+		else:
+			raise AttributeError
+
+class Area:
+	# TODO - how do we handle inns?
+	# TODO - need to handle treasure chest areas for rogue etc
+	def __init__(self, name, color, treasure_chest=False, gateway=False,neighbors=set()):
+		if color in colors:
+			self.color     = color
+			self.name      = name
+			self.treasure_chest = treasure_chest
+			self.gateway   = gateway
+			self.neighbors = neighbors
+			self.minions   = set()
+			self.general   = set()
+			self.crystals  = set()
+			self.defenders = set()
+		else:
+			raise AttributeError
+
+
+class StateMachine:
+	def __init__(self):
+		self.day_phase = collections.deque(['day','evening','night'],maxlen=3)
+		#self.day_phase.rotate(-1) to rotate through and 0th element can be current maybe make its own function to encapsulate this
+		self.allowed_actions=None
+		self.player_list = None
+		# allowed actions for day
+			# move
+			# fight minions
+			# done (moves to next phase)
+			# show board
+			# play special cards (instants that can be played any time)
+		#allowed actions evening
+			# done (moves to next phase) but only after  taking two cards and if needed discarding
+			# show board
+			# take two hero card
+			# discard down to hand limit (10)
+			# play special cards (instants that can be played any time)
+		# allowed actions night
+			# done (moves to next phase) But only after darkness spreads tasks
+			# show board
+			# take correct number of cards and do correct actions on them
+			# play special cards (instants that can be played any time)
+		# any time actions
+			# show board
+			# save bookmark to this game (dump state and serialize?)
+			# quit
+
+#TODO Think about logging each action (hero or darkness spreads) somewhere so we can replay game?
+			
+class action:
+	def __init__(self,name,function,*args):
+		self.action=function(*args)
+
+class GameController:
+	def __init__(self):
+		self.possible_actions = None
+		self.allowed_actions  = None
+
+		self.initialize_actions()
+		self.game_data = Game()
+
+	
+		
+	def initialize_actions(self):
+		self.possible_actions["goto_next_phase"] = game_data.advance_phase 
+
+
+class Game:
+	def __init__(self):
+		self.minions = dict()
+		self.minions["red"]   = None;
+		self.minions["green"] = None;
+		self.minions["blue"]  = None;
+		self.minions["black"] = None;
+
+		self.unused_minions = dict()
+		self.unused_minions["red"] = set()
+		self.unused_minions["green"] = set()
+		self.unused_minions["blue"] = set()
+		self.unused_minions["black"] = set()
+
+		self.crystals = set()
+		self.unused_crystals = set()
+		self.generals=dict()
+		
+		self.areas         = None
+
+		self.ds_deck = None
+		self.hc_deck = None
+		self.qc_deck = None
+
+		self.war_status = 0
+
+		self.initialize_minions()
+		self.initialize_areas()
+		self.initialize_crystals()
+		self.initialize_generals()
+
+	def dump_game_data(self):
+		print "Unused Minions:"
+		print "\tRed: (" + str(len(self.unused_minions["red"])) + ")"
+		print "\tGreen: (" + str(len(self.unused_minions["green"])) + ")"
+		print "\tBlue: (" + str(len(self.unused_minions["blue"])) + ")"
+		print "\tBlack: (" + str(len(self.unused_minions["black"])) + ")"
+		print "Unused Crystals: (" + str(len(self.unused_crystals)) + ")" 
+		print "Areas:"
+		for area_name in self.areas:
+			area = self.areas[area_name]
+			print area.name + ":"
+			print "\tminions: ",
+			for minion in area.minions:
+				print minion.color,
+			print " "
+			print "\tcrystals: " + str(len(area.crystals))
+
+	def initialize_minions(self, minion_count=20):
+		self.minions["red"] = [Minion("red",self.unused_minions["red"])     for _ in range(minion_count)]
+		self.minions["green"] = [Minion("green",self.unused_minions["green"]) for _ in range(minion_count)]
+		self.minions["black"] = [Minion("black",self.unused_minions["black"]) for _ in range(minion_count)]
+		self.minions["blue"] = [Minion("blue",self.unused_minions["blue"])   for _ in range(minion_count)]
+
+	def initialize_crystals(self, crystal_count=12):
+		self.crystals = [Crystal(self.unused_crystals)   for _ in range(crystal_count)]
+
+	def initialize_generals(self):
+		self.generals["Sapphire"] = General(self.areas["Blizzard Mountains"],"Sapphire",5)
+		self.generals["Varkalok"] = General(self.areas["Dark Woods"],"Sapphire",5)
+		self.generals["Gorgutt"] = General(self.areas["Thorny Woods"],"Gorgutt",5)
+		self.generals["Blazaarg"] = General(self.areas["Scorpion Canyon"],"Balazaarg",5)
+
+
+	def initialize_areas(self):
+		#Eventually make this read in from config file?
+		self.areas = dict()
+		self.areas['Amarak Peak']      = Area('Amarak Peak','blue')
+		self.areas['Ancient Ruins']      = Area('Ancient Ruins','red')
+		self.areas['Angel Tear Falls']   = Area('Angel Tear Falls','black')
+		self.areas['Blizzard Mountains']      = Area('Blizzard Mountains','blue')
+		self.areas['Blood Flats']      = Area('Blood Flats','red')
+		self.areas['Bounty Bay']         = Area('Bounty Bay','blue',treasure_chest=True)
+		self.areas['Brookdale Village']      = Area('Brookdale Village','black')
+		self.areas['Chimera Inn']      = Area('Chimera Inn','purple')
+		self.areas['Crystal Hills']      = Area('Crystal Hills','blue')
+		self.areas['Cursed Plateau']      = Area('Cursed Plateau','red')
+		self.areas['Dancing Stones']     = Area('Dancing Stones','black',gateway=True)
+		self.areas['Dragons Teeth Range']      = Area('Dragons Teeth Range','blue')
+		self.areas['Dark Woods']      = Area('Dark Woods','black')
+		self.areas['Eagle Nest Inn']      = Area('Eagle Nest Inn','purple')
+		self.areas['Eagle Peak Pass']      = Area('Eagle Peak Pass','blue')
+		self.areas['Enchanted Glade']      = Area('Enchanted Glade','black')
+		self.areas['Father Oak Forest']  = Area('Father Oak Forest','green')
+		self.areas['Fire River']      = Area('Fire River','black')
+		self.areas['Ghost Marsh']      = Area('Ghost Marsh','red')
+		self.areas['Golden Oak Forest']      = Area('Golden Oak Forest','green')
+		self.areas['Greenleaf Village'] = Area('Greenleaf Village','green') 
+		self.areas['Gryphon Forest']      = Area('Gryphon Forest','green')
+		self.areas['Gryphon Inn']      = Area('Gryphon Inn','purple')
+		self.areas['Heavens Glade']      = Area('Heavens Glade','green')
+		self.areas['Land of Amazons']      = Area('Land of Amazons','black',treasure_chest=True)
+		self.areas['Mccorm Highlands']      = Area('Mccorm Highlands','black',treasure_chest=True)
+		self.areas['Mermaid Harbor']      = Area('Mermaid Harbor','black',treasure_chest=True)
+		self.areas['Minotaur Forest']      = Area('Minotaur Forest','green')
+		self.areas['Monarch City']       = Area('Monarch City','purple',treasure_chest=True)
+		self.areas['Mountains of Mist']      = Area('Mountains of Mist','blue')
+		self.areas['Orc Valley']         = Area('Orc Valley','red')
+		self.areas['Pleasant Hill']      = Area('Pleasant Hill','red',treasure_chest=True)
+		self.areas['Raven Forest']      = Area('Raven Forest','green')
+		self.areas['Rock Bridge Pass']      = Area('Rock Bridge Pass','blue')
+		self.areas['Scorpion Canyon']      = Area('Scorpion Canyon','red')
+		self.areas['Seabird Port']      = Area('Seabird Port','black',treasure_chest=True)
+		self.areas['Seagull Lagoon']      = Area('Seagull Lagoon','blue')
+		self.areas['Serpent Swamp']      = Area('Serpent Swamp','red')
+		self.areas['Thorny Woods']      = Area('Thorny Woods','green')
+		self.areas['Unicorn Forest']      = Area('Unicorn Forest','green')
+		self.areas['Whispering Woods']      = Area('Whispering Woods','green')
+		self.areas['Withered Hills']      = Area('Withered Hills','red')
+		self.areas['Windy Pass']      = Area('Windy Pass','red')
+		self.areas['Wolf Pass']          = Area('Wolf Pass','blue')
+		self.areas['Wyvern Forest']      = Area('Wyvern Forest','green')
+
+		self.areas['Amarak Peak'].neighbors   = [self.areas['Eagle Peak Pass'],
+								self.areas['Mccorm Highlands'],
+								self.areas['Ghost Marsh'],
+								self.areas['Thorny Woods']]
+		self.areas['Ancient Ruins'].neighbors   = [self.areas['Whispering Woods'],
+								self.areas['Heavens Glade'],
+								self.areas['Greenleaf Village']]
+		self.areas['Angel Tear Falls'].neighbors   = [self.areas['Bounty Bay'],
+								self.areas['Pleasant Hill'],
+								self.areas['Fire River'],
+								self.areas['Dragons Teeth Range'],
+								self.areas['Raven Forest']]
+		self.areas['Blizzard Mountains'].neighbors = [self.areas['Withered Hills'],
+								self.areas['Heavens Glade']]
+		self.areas['Blood Flats'].neighbors = [self.areas['Scorpion Canyon'],
+								self.areas['Raven Forest'],
+								self.areas['Unicorn Forest'],
+								self.areas['Brookdale Village']]
+		self.areas['Bounty Bay'].neighbors         = [self.areas['Monarch City'],
+								self.areas['Greenleaf Village'],
+								self.areas['Angel Tear Falls'],
+								self.areas['Mermaid Harbor']]
+		self.areas['Brookdale Village'].neighbors     = [self.areas['Blood Flats'],
+								self.areas['Unicorn Forest'],
+								self.areas['Rock Bridge Pass'],
+								self.areas['Seabird Port'],
+								self.areas['Father Oak Forest'],
+								self.areas['Pleasant Hill']]
+		self.areas['Chimera Inn'].neighbors     = [self.areas['Withered Hills']]
+		self.areas['Dancing Stones'].neighbors     = [self.areas['Monarch City'],
+								self.areas['Orc Valley'],
+								self.areas['Greenleaf Village']]
+		self.areas['Father Oak Forest'].neighbors  = [self.areas['Monarch City'],
+								self.areas['Wolf Pass']]
+		self.areas['Greenleaf Village'].neighbors = [self.areas['Monarch City'],
+								self.areas['Dancing Stones'],
+								self.areas['Bounty Bay']]
+		self.areas['Monarch City'].neighbors       = [self.areas['Father Oak Forest'],
+								self.areas['Wolf Pass'],
+								self.areas['Orc Valley'],
+								self.areas['Dancing Stones'],
+								self.areas['Greenleaf Village'],
+								self.areas['Bounty Bay']]
+		self.areas['Orc Valley'].neighbors         = [self.areas['Monarch City'],
+								self.areas['Wolf Pass'],
+								self.areas['Dancing Stones']]
+		self.areas['Pleasant Hill'].neighbors      = [self.areas['Angel Tear Falls'],
+								self.areas['Father Oak Forest']]
+		self.areas['Raven Forest'].neighbors       = [self.areas['Scorpion Canyon'],
+								self.areas['Blood Flats'],
+								self.areas['Pleasant Hill'],
+								self.areas['Angel Tear Falls']]
+		self.areas['Wolf Pass'].neighbors          = [self.areas['Monarch City'],
+								self.areas['Father Oak Forest'],
+								self.areas['Orc Valley']]
+
+	def overrun_area(self,color,area):
+		print "An overrun was triggered in " + area.name + " || color (" + color + ")"
+		for neighbor in area.neighbors:
+			print "An overrun flowed into " + neighbor.name + " || color (" + color + ")"
+			self.add_minion_to_area(color, neighbor, True)
+
+
+	def taint_area(self, area):
+		print "A taint was triggered in " + area.name 
+		try:
+			crystal = self.unused_crystals.pop()
+			area.crystals.add(crystal)
+			crystal.location = area
+		except KeyError, e:
+			print "Game over, no more crystals to place"
+			sys.exit(0)
+
+	def add_minion_to_area(self,color, area, overrun_triggered = False):
+		# TODO if unused piles have no minions left, than check if they are on quest cards
+		taint_done = False
+		if (area.name == 'Monarch City'): # so no overflow or taint, but we could lose the game if there are 5
+			if (len(area.minions) >=5):
+				print "Game over, Monarch city has been overrun with minions"
+				sys.exit(0)
+			else:
+				minion = self.unused_minions[color].pop()
+				minion.location = area
+				area.minions.add(minion)
+		else: #Monarch city is different from how we handle everything else
+			if (len(area.minions) >= 3):
+				self.taint_area(area)
+				taint_done = True
+				if (overrun_triggered == False):
+					self.overrun_area(color,area)
+
+			else:
+				try:
+					minion = self.unused_minions[color].pop()
+					minion.location = area
+					area.minions.add(minion)
+				except KeyError, e:
+					#TODO need to check for minions quest cards now or we lose the game
+					print "Game over, no more " + color + "minions to place"
+					sys.exit(0)
+
+			if (color == 'red' and len(area.minions) >= 3 and taint_done == False):
+				# keep this separate from overrun logic so its not too messy
+				#This is for demon overruns only, the minion should have already been placed unlike the other taint code
+				#TODO , need to see if other already placed minions are all red
+				red_count = 0
+				for i in area.minions:
+					if i.color=='red':
+						red_count += 1
+				if red_count >= 3:
+					print "\tDemon taint in " + area.name
+					self.taint_area(area)
+
+	def darknessSpreads(self):
+		# draw card, do actions
+		# if middle or late war draw another and do actions, but skip non-general move actions
+		if self.war_status[current_war_state] != 'early':
+			print "hello"
+		# if late war draw another and do actions, but skip non-general move actions
+		if self.war_status[current_war_state] == 'late':
+			print "hello"
+
+# vim: tabstop=4 shiftwidth=4 expandtab
+
---
+++ tests.py
@@ -0,0 +1,101 @@
+import random
+import unittest
+import dotr
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_shuffle(self):
+        # make sure the shuffled sequence does not lose any elements
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, range(10))
+
+        # should raise an exception for an immutable sequence
+        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+    def test_choice(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
+
+    def test_adding_minions(self):
+	print "Test Adding minions to an area"
+	game_data = dotr.Game()
+	area = game_data.areas["Pleasant Hill"]
+        self.assertTrue(len(area.minions) == 0)
+	game_data.add_minion_to_area("red", area)
+	print "Asserting that minions in area = 1"
+        self.assertTrue(len(area.minions) == 1)
+	print "Asserting that unused red minions = 19 (" + str(len(game_data.unused_minions["red"])) + ")"
+        self.assertTrue(len(game_data.unused_minions["red"]) == 19)
+
+    def test_max_minions_per_area(self):
+	print "Test max minions per area"
+	game_data = dotr.Game()
+	area = game_data.areas["Father Oak Forest"]
+	print "Testing that area starts with no minions"
+        self.assertTrue(len(area.minions) == 0)
+	game_data.add_minion_to_area("black", area)
+	game_data.add_minion_to_area("red", area)
+	game_data.add_minion_to_area("black", area)
+	print "Asserting that minions = 3"
+        self.assertTrue(len(area.minions) == 3)
+	print "Asserting that minions still = 3"
+	game_data.add_minion_to_area("red", area)
+        self.assertTrue(len(area.minions) == 3)
+	print "Asserting that minions still = 3"
+	game_data.add_minion_to_area("blue", area)
+        self.assertTrue(len(area.minions) == 3)
+	print "Asserting that unused black minions = 18 (" + str(len(game_data.unused_minions["black"])) + ")"
+        self.assertTrue(len(game_data.unused_minions["black"]) == 18)
+	print "Asserting that unused blue minions = 19 (" + str(len(game_data.unused_minions["blue"])) + ")"
+        self.assertTrue(len(game_data.unused_minions["blue"]) == 18)
+
+# test over run
+# test crystal placement
+# test game ends when out of minions of a color, and a new one is attempted to be placed (we mightbe doing this wrong in the code)
+# test game ends when out of crystals
+# test monarch city can get up to 5 minions, game ends when at 5 or maybe when one more than 5 attempts to get placed
+
+#
+#
+#for area_name in game_data.areas:
+#	an_area = game_data.areas[area_name]
+#	#print "Number of unused crystals: " + str(len(game_data.unused_crystals))
+#	#print "Area name is " + str(an_area.name)
+#	print "Adding a new minion ---------------------------------------------- " + str(an_area.name) 
+#	game_data.add_minion_to_area("black", an_area) 
+#	game_data.dump_game_data()
+#	print "Adding a new minion ---------------------------------------------- " + str(an_area.name) 
+#	game_data.add_minion_to_area("black", an_area) 
+#	game_data.dump_game_data()
+#	print "Adding a new minion ---------------------------------------------- " + str(an_area.name) 
+#	game_data.add_minion_to_area("black", an_area) 
+#	game_data.dump_game_data()
+#	print "Adding a new minion ---------------------------------------------- " + str(an_area.name) 
+#	game_data.add_minion_to_area("black", an_area) 
+#	game_data.dump_game_data()
+#	#print "Adding a new minion ----------------------------------------------"
+#	#game_data.add_minion_to_area("black", an_area) 
+#	#game_data.dump_game_data()
+#	#print "Adding a new minion ----------------------------------------------"
+#	#game_data.add_minion_to_area("black", an_area) 
+#	#game_data.dump_game_data()
+#	#print "Adding a new minion ----------------------------------------------"
+#	#game_data.add_minion_to_area("black", an_area) 
+#	#game_data.dump_game_data()
+#	#print "Number of unused black minions: " + str(len(game_data.unused_minions["black"]))
+#	#print "Number of minions in area: " + str(len(an_area.minions))
+#	#print "Area minions set: " + str(an_area.minions)
+#
+#
+if __name__ == '__main__':
+    unittest.main()
